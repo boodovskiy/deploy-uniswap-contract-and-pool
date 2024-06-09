8f3a3819512381e308bd9adede7847cef0976ec3
boodovskiy <budovskiy@specialcase.net>
2024-06-09 11:36:09 +0300

    Deploy token contracts and mint (Usdc, Tether and WBTC)

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   networks: {
     sepolia: {
       url: process.env.INFURA_URL_SEPOLIA,
-      accounts: [process.env.PRIVATE_KEY]
+      accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY_2]
     },
   },
 };
